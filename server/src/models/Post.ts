e8ee0c5c4027d18731975d9e8dad5ef1930d831e
Anna Lebid <annalez16@gmail.com>
2023-11-22 22:02:28 +0200

    update Post schema

--- server/src/models/Post.ts
+++ server/src/models/Post.ts
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-const PostSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  prompt: { type: String, required: true },
-  photoUrl: { type: String, required: true },
-});
+const PostSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    prompt: { type: String, required: true },
+    photoUrl: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export const Post = mongoose.model("Post", PostSchema);
